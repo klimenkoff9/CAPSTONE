229a51ab479cc4b8caf9bec33c3bbcd4466a1371
jahedev <jahedev@gmail.com>
2021-01-26 16:09:53 -0500

    remove .eslintcache

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import RoutesContainer from '../components/AllPlayers/routes/RoutesContainer';
+import React, { Component } from 'react'
+import './App.css'
+import RoutesContainer from '../components/AllPlayers/routes/RoutesContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app-header">
+      <div className='app'>
+        <header className='app-header'>
+          <h1>It works!</h1>
           <RoutesContainer />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
