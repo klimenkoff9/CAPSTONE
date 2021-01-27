f3d08631f16ebaf1c5d4e0b00ae27e3cbbcc055c
jahedev <jahedev@gmail.com>
2021-01-26 19:53:21 -0500

    created login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import HalfHeaderBG from './HalfHeaderBG'
+import { Link } from 'react-router-dom'
+
+import '../css/login.css'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <HalfHeaderBG imgdiv='half-bg-login' title='Login' />
-        <h1>It's working</h1>
+      <div className='bg-login'>
+        <div className='login-container'>
+          <div className='form-container'>
+            <h1>Please Login</h1>
+            <form>
+              <div className='form-control'>
+                <input type='text' required />
+                <label>
+                  <span>Email</span>
+                </label>
+              </div>
+
+              <div className='form-control'>
+                <input type='password' required />
+                <label>
+                  <span>Password</span>
+                </label>
+              </div>
+
+              <button type='button' className='btn'>
+                Login
+              </button>
+              <p className='text'>
+                Don't have an account? <Link to='/signup'>Signup</Link>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
